61c7124aef3b9c8e3b99ef7482119ba8e498ed83
Jomanah Bin Faris <JomanahBinfaris@gmail.com>
2022-10-23 21:25:15 +0300

    Update pubspec.yaml

--- faydh/lib/awarenessPost.dart
+++ faydh/lib/awarenessPost.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
