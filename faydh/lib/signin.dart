d1067b04ae2e99d4193009ff8be95861fecb9f01
shaden789 <52710817+shaden789@users.noreply.github.com>
2022-10-29 23:47:01 +0300

    Update signin.dart

--- faydh/lib/signin.dart
+++ faydh/lib/signin.dart
@@ -1,13 +1,10 @@
 import 'package:faydh/SignUp_Form.dart';
-import 'package:faydh/awarenessPost.dart';
 import 'package:faydh/home_page.dart';
+import 'package:faydh/services/auth_methods.dart';
+import 'package:faydh/utilis/utilis.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/animation/animation_controller.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/ticker_provider.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:line_icons/line_icons.dart';
 
 import 'forget-password.dart';
 
@@ -19,9 +16,18 @@ class signInSreen extends StatefulWidget {
 }
 
 class _signInSreenState extends State<signInSreen> {
-  int _selectedIndex = 1;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
+  final _formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -39,123 +45,141 @@ class _signInSreenState extends State<signInSreen> {
         width: MediaQuery.of(context).size.width,
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 30),
-              Image.asset(
-                'assets/imgs/logo.png',
-                width: 250,
-                height: 250,
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                textAlign: TextAlign.right,
-                decoration: InputDecoration(
-                  hintText: 'اسم المستخدم',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 30),
+                Image.asset(
+                  'assets/imgs/logo.png',
+                  width: 250,
+                  height: 250,
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  controller: _emailController,
+                  textAlign: TextAlign.right,
+                  decoration: InputDecoration(
+                    hintText: 'البريد الإلكتروني',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                textAlign: TextAlign.right,
-                decoration: InputDecoration(
-                  hintText: 'كلمة المرور',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+                const SizedBox(height: 30),
+                TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  textAlign: TextAlign.right,
+                  decoration: InputDecoration(
+                    hintText: 'كلمة المرور',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xffd6ecd0),
+                        width: 1.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              MaterialButton(
-                  elevation: 5.0,
-                  color: const Color(0xff1a4d2e),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 80),
-                  child: const Text(
-                    'تسجيل الدخول',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                const SizedBox(height: 50),
+                MaterialButton(
+                    elevation: 5.0,
+                    color: const Color(0xff1a4d2e),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 80),
+                    child: const Text(
+                      'تسجيل الدخول',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  shape: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: BorderSide.none,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const HomePage()),
-                    );
-                  }),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return const Password();
-                  }));
-                },
-                child: const Text('نسيت كلمة المرور؟',
-                    style: TextStyle(
-                      color: Color(0xff201a19),
-                      fontSize: 20,
-                    )),
-              ),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return const SignupForm();
-                  }));
-                },
-                child: const Text('مستخدم جديد؟ تسجيل',
-                    style: TextStyle(
-                      color: Color(0xff201a19),
-                      fontSize: 20,
-                    )),
-              ),
-            ],
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide.none,
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        AuthMethods()
+                            .loginUser(
+                                email: _emailController.text,
+                                password: _passwordController.text)
+                            .then((value) {
+                          showSnackBar(value, context);
+                          if (value == "success") {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomePage()),
+                            );
+                          }
+                        });
+                      }
+                    }),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return const PasswordReset();
+                    }));
+                  },
+                  child: const Text('نسيت كلمة المرور؟',
+                      style: TextStyle(
+                        color: Color(0xff201a19),
+                        fontSize: 20,
+                      )),
+                ),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return const SignupForm();
+                    }));
+                  },
+                  child: const Text('مستخدم جديد؟ تسجيل',
+                      style: TextStyle(
+                        color: Color(0xff201a19),
+                        fontSize: 20,
+                      )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
