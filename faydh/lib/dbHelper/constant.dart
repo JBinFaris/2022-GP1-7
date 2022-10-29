e7a16235dcdae81c5f31770182770540f724a9ce
Jomanah Bin Faris <JomanahBinfaris@gmail.com>
2022-10-29 21:25:29 +0300

    change to firebase

--- faydh/lib/dbHelper/constant.dart
+++
@@ -1,5 +0,0 @@
-const MONGO_CONN_URL =
-    "mongodb+srv://FAYDH:1234567890@faydh.arvxaqe.mongodb.net/FAYDH?retryWrites=true&w=majority";
-
-const USER_COLLECTION = "users";
-const AWARPOST_COLLECTION = "awarpost";
