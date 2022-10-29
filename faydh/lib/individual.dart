514ce2c2f3929c7516dbeab8b36f4ce6e6230f3a
alanoudNb <98782300+alanoudNb@users.noreply.github.com>
2022-10-29 23:09:03 +0300

    update individual design
    update individual design

--- faydh/lib/individual.dart
+++ faydh/lib/individual.dart
@@ -13,6 +13,7 @@ class _individualPageState extends State<individual> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -22,53 +23,53 @@ class _individualPageState extends State<individual> {
               colors: [Color.fromARGB(142, 26, 77, 46), Color(0xffd6ecd0)]),
         ),
         alignment: Alignment.center,
-        padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-        child: Column(children: <Widget>[
-          Align(
-            alignment: Alignment.topCenter,
-            child: ClipOval(
-              child: Image.asset(
-                'assets/imgs/logo.png',
-                width: 140,
-                height: 140,
-                fit: BoxFit.cover,
+        padding: EdgeInsets.only(top: 90.0, left: 20.0, right: 20.0),
+        child: Expanded(
+          child: Column(children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/imgs/logo.png',
+                  width: 300,
+                  height: 300,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 140.0, left: 20.0, right: 20.0),
-            child: ElevatedButton(
+            Container(
+              padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('مستهلك'),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    primary: Color(0xFF1A4D2E),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 110.0, vertical: 25.0),
+                    textStyle:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
               onPressed: () {},
-              child: Text('مستهلك'),
+              child: Text('مستفيد'),
               style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20)),
-                  primary: Color.fromARGB(226, 29, 92, 76),
+                  primary: Color(0xFF1A4D2E),
                   padding:
                       EdgeInsets.symmetric(horizontal: 110.0, vertical: 25.0),
                   textStyle:
                       TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('مستفيد'),
-            style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                primary: Color.fromARGB(226, 29, 92, 76),
-                padding:
-                    EdgeInsets.symmetric(horizontal: 110.0, vertical: 25.0),
-                textStyle:
-                    TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          )
-        ]),
+            )
+          ]),
+        ),
       ),
-
-
     );
   }
 }
