d73469b39a12af3eb42d133d1545ae719d05d564
shaden789 <52710817+shaden789@users.noreply.github.com>
2022-11-05 20:24:04 +0300

    shaden's edit

--- faydh/lib/main.dart
+++ faydh/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:faydh/signin.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
