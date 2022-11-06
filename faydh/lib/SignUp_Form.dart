b00853c7ef0ac6d2c4a7d8ba81069c065c1ef61a
shaden789 <52710817+shaden789@users.noreply.github.com>
2022-11-06 12:04:45 +0300

    Terms & conditions

--- faydh/lib/SignUp_Form.dart
+++ faydh/lib/SignUp_Form.dart
@@ -1,3 +1,4 @@
+import 'package:faydh/components/dialogs/%20terms_and_condition.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:faydh/services/auth_methods.dart';
@@ -23,7 +24,6 @@ class _SignupFormState extends State<SignupForm> {
   bool? _isChecked = false;
   bool? seen = false;
   String? selectedValue;
-
 
   @override
   void dispose() {
@@ -85,7 +85,6 @@ class _SignupFormState extends State<SignupForm> {
   // 3/4: Strong
   //   1:   Great
 
-  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -151,13 +150,13 @@ class _SignupFormState extends State<SignupForm> {
                           color: Color.fromARGB(255, 18, 57, 20),
                         ),
                         contentPadding:
-                           const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
                           borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
                         ),
                         filled: true,
                         hintStyle: TextStyle(color: Colors.grey[800]),
@@ -167,7 +166,6 @@ class _SignupFormState extends State<SignupForm> {
                         ),
 
                         // contentPadding: EdgeInsets.only(left:230),
-                        
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -196,25 +194,25 @@ class _SignupFormState extends State<SignupForm> {
                             const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
-                           borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
                         ),
-                         disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
+                        disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
                         filled: true,
                         hintStyle: TextStyle(color: Colors.grey[800]),
                         label: const Align(
@@ -223,7 +221,6 @@ class _SignupFormState extends State<SignupForm> {
                         ),
 
                         // contentPadding: EdgeInsets.only(left:230),
-                        
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -260,24 +257,24 @@ class _SignupFormState extends State<SignupForm> {
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
                           borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
                         ),
-                         disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
+                        disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
                         filled: true,
                         hintStyle: TextStyle(color: Colors.grey[800]),
                         label: const Align(
@@ -286,7 +283,6 @@ class _SignupFormState extends State<SignupForm> {
                         ),
 
                         // contentPadding: EdgeInsets.only(left:230),
-                        
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -307,8 +303,16 @@ class _SignupFormState extends State<SignupForm> {
                     padding: EdgeInsets.only(top: 3, bottom: 3),
                     child: Align(
                       alignment: Alignment.centerRight,
-                      child: Text( "كلمة المرور يجب ان تتكون من: حرف كبير ،حرف صغير ،رقم ورمز مميز", style: TextStyle( color: Color.fromARGB(255, 0, 0, 0) , fontSize:13, ) , textAlign: TextAlign.right, ),
-                    ),),
+                      child: Text(
+                        "كلمة المرور يجب ان تتكون من: حرف كبير ،حرف صغير ،رقم ورمز مميز",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 13,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: LinearProgressIndicator(
@@ -342,25 +346,25 @@ class _SignupFormState extends State<SignupForm> {
                             const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
-                           borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
                         ),
-                         disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
+                        disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color(0xffd6ecd0),
+                            width: 1.0,
+                          ),
+                        ),
                         filled: true,
                         hintStyle: TextStyle(color: Colors.grey[800]),
                         label: const Align(
@@ -369,7 +373,6 @@ class _SignupFormState extends State<SignupForm> {
                         ),
 
                         // contentPadding: EdgeInsets.only(left:230),
-                        
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -402,29 +405,29 @@ class _SignupFormState extends State<SignupForm> {
                                 size: 30,
                                 color: Color.fromARGB(255, 18, 57, 20),
                               ),
-                              contentPadding:
-                                  const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  20.0, 10.0, 20.0, 10.0),
                               enabledBorder: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20),
-                                  borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                        ),
-                         disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(
-                      color: Color(0xffd6ecd0),
-                      width: 1.0,
-                    ),
-                  ),
+                                borderSide: const BorderSide(
+                                  color: Color(0xffd6ecd0),
+                                  width: 1.0,
+                                ),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                  color: Color(0xffd6ecd0),
+                                  width: 1.0,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: const BorderSide(
+                                  color: Color(0xffd6ecd0),
+                                  width: 1.0,
+                                ),
+                              ),
                               filled: true,
                               hintStyle: TextStyle(color: Colors.grey[800]),
                               label: const Align(
@@ -432,8 +435,8 @@ class _SignupFormState extends State<SignupForm> {
                                   child: Text('نوع المستخدم')),
                               // contentPadding: EdgeInsets.only(left:230),
                             ),
-                            items: userTypes.map<DropdownMenuItem<String>>(
-                                (String value) {
+                            items: userTypes
+                                .map<DropdownMenuItem<String>>((String value) {
                               return DropdownMenuItem<String>(
                                 value: value,
                                 child: Text(value),
@@ -455,34 +458,24 @@ class _SignupFormState extends State<SignupForm> {
                             },
                           ))),
                   //checkbox
+
                   Align(
                     alignment: Alignment.centerRight,
                     child: Padding(
                       padding: const EdgeInsets.only(top: 5, bottom: 2),
-                      child: CheckboxListTile(
-                        title: const Text(
-                          "اوافق على الاحكام والشروط",
-                          textAlign: TextAlign.right,
-                        ),
-                        value: _isChecked,
-                        activeColor: const Color.fromARGB(255, 18, 57, 20),
-                        onChanged: (newBool) {
-                          setState(() {
-                            _isChecked = newBool;
-                            seen = true;
-                          });
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return const TermsAndConditions();
+                          }));
                         },
-                        subtitle: seen == true && _isChecked == false
-                            ? const Padding(
-                                padding: EdgeInsets.fromLTRB(12.0, 0, 0, 0),
-                                child: Text(
-                                  'يجب الموافقة على الاحكام والشروط',
-                                  style: TextStyle(
-                                      color: Color(0xFFe53935), fontSize: 12),
-                                  textAlign: TextAlign.right,
-                                ),
-                              )
-                            : null,
+                        child: const Text(
+                            'تسجيل دخولك يعني موافقتك على الشروط والأحكام',
+                            style: TextStyle(
+                              color: Color(0xff201a19),
+                              fontSize: 15,
+                            )),
                       ),
                     ),
                   ),
@@ -519,8 +512,7 @@ class _SignupFormState extends State<SignupForm> {
                                       .then((value) {
                                     if (value == "success") {
                                       Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (context) {
+                                          MaterialPageRoute(builder: (context) {
                                         return const HomePage();
                                       }));
                                     }
@@ -531,8 +523,7 @@ class _SignupFormState extends State<SignupForm> {
                               child: const Text(
                                 'تسجيل',
                                 style: TextStyle(
-                                    color:
-                                        Color.fromARGB(255, 255, 255, 255)),
+                                    color: Color.fromARGB(255, 255, 255, 255)),
                               ),
                             ),
                           ))),
---
+++ faydh/lib/components/dialogs/ terms_and_condition.dart
@@ -0,0 +1,171 @@
+import 'package:faydh/SignUp_Form.dart';
+import 'package:flutter/material.dart';
+
+import 'package:faydh/services/auth_methods.dart';
+import 'package:faydh/signin.dart';
+import 'package:faydh/utilis/utilis.dart';
+import 'package:flutter/material.dart';
+
+class TermsAndConditions extends StatefulWidget {
+  const TermsAndConditions({Key? key}) : super(key: key);
+
+  @override
+  State<TermsAndConditions> createState() => _TermsAndConditionsState();
+}
+
+String Terms = "sssshhhaaaddeeeennn";
+
+class _TermsAndConditionsState extends State<TermsAndConditions> {
+  @override
+  @override
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 70, right: 5, left: 5),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return const SignupForm();
+                  }));
+                },
+                icon: const Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: Color.fromARGB(255, 18, 57, 20),
+                )),
+          )),
+
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              stops: [0.1, 0.9],
+              colors: [Color.fromARGB(142, 26, 77, 46), Color(0xffd6ecd0)]),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 00,
+        ),
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 30),
+              Image.asset(
+                'assets/imgs/logo.png',
+                width: 150,
+                height: 150,
+              ),
+              const Text(
+                "يشكل استخدام هذا التطبيق قبولاً بالشروط والأحكام الواردة فيه، وتحتفظ فيض، بالحق في مراجعة شروط الاستخدام المذكورة في أي وقت، وتلتزم بالإعلان عن أي تعديل أو تغيير في تلك الشروط على تطبيقها.",
+                textAlign: TextAlign.right,
+              ),
+              const Text(
+                "شروط الاستخدام: ",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "تطبق قواعد وشروط استخدام تطبيق فيض على جميع زوار ومستخدمي التطبيق. ويجوز إيقاف أو منع أو إنهاء استخدام التطبيق في حال حدوث انتهاك من قبل أحد المستخدمين، أو في حال توفرت أسباب تدعو للاعتقاد بأن أحد المستخدمين قد انتهك وخالف شروط وقواعد الاستخدام.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "▪محاولة إجراء اختبار أو مسح أو فحص لإمكانية إصابة نقاط الضعف في النظام أو انتهاك سلامة الإجراءات أو توثيقها دون تصريح رسمي.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "▪محاولة التدخل في الخدمة المقدمة لأي مستخدم بما في ذلك على سبيل المثال وليس الحصر، عن طريق وضع فيروس على التطبيق أو زيادة الحمل عليه أو إغراقه بالرسائل الإلكترونية أو تحطيمه.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "▪إن مخالفة قواعد الاستخدام وانتهاك النظام أو الشبكة يعرض المخالف للمسؤولية المدنية والجنائية وسيتم مباشرة التحقيق في الحالات التي قد تنطوي على مثل هذه المخالفات والانتهاكات وملاحقة المتسبب فيها قضائياً.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "▪إرسال رسائل إلكترونية إلى التطبيق غير مرغوب فيها، بما ذلك عمليات الدعاية، أو الإعلان عن المنتجات أو الخدمات، أو تزييف أي عنوان الكتروني أو جزء من معلومات العنوان في أي رسالة إلكترونية أو إرسال رسائل مجموعات إخبارية.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "حقوق الطبع:",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "تعتبر كافة الحقوق بما في ذلك حقوق الطبع وقاعدة البيانات ومحتوياته، مملوكة للتطبيق أو مرخصة باسمها أو مستخدمة من قبلها، وفقاً لما تجيزه القوانين السارية.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "حقوق الملكية الفكرية:",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "يقر المستخدم ويوافق على أن كافة المحتويات والمواد المتوفرة والمعروضة في هذا التطبيق تخضع للحماية بواسطة القوانين التي تحكم حقوق الطبع والأسرار التجارية وحقوق الملكية الفكرية الأخرى، فإن المستخدم يوافق على أن لا يبث أو يعرض أو ينفذ أو ينشر أو يعدل أو يحرر أو يُنشئ أية أعمال مشتقة من تلك المواد أو المحتويات.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "يجوز للمستخدم أن يطبع أو يحمل نسخة من المواد أو المحتويات في هذا التطبيق، في أي حاسب آلي شخصي لاستخدامه الشخصي أو لاستخدام غير تجاري، وذلك شريطة أن يحافظ على كافة حقوق الطبع وحقوق الملكية الأخرى دون تغيير، ويحظر استرجاع البيانات أو أية محتويات أخرى بطريقة منظمة من هذا التطبيق لإنشاء أو جمع تشكيلة أو مجموعة أو قاعدة بيانات أو دليل بدون إذن خطي. ويحظر استخدام المحتويات أو المواد المنشورة في هذا التطبيق لأي غرض لا يكون مسموحاً به صراحة ضمن الشروط الخاصة بالاستخدام.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "إخلاء المسؤولية:",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "لا تتحمل فيض تحت أي ظرف من الظروف مسؤولية أية أضرار مباشرة أو غير مباشرة أو عرضية أو تبعية أو خاصة أو استثنائية تنشأ عن استخدام أو عدم القدرة على استخدام هذا التطبيق. ويطبّق هذا الشرط سواء كانت المسؤولية المدعى بها مسؤولية تعاقدية أو تقصيرية أو مسؤولية ناشئة عن الإهمال أو أي شكل آخر من أشكال المسؤولية القانونية. في حال انقطاع الخدمة نتيجة أسباب قهرية، فإن “فيض” لا تتحمل مسؤولية تلك الأعطال أو أية أضرار متعلقة بها.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "القوانين والشرائع السائدة:",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "أي نزاع أو مطالبة تنشأ عن أو فيما يتعلق بهذا التطبيق يجب أن تخضع وتفسر وفقا لقوانين المملكة العربية السعودية.",
+                textDirection: TextDirection.rtl,
+              ),
+              const Text(
+                "سياسة التسليم / سياسة الشحن:",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "لا يتم تقديم أو عرض أي منتجات أو مواد مادية في التطبيق وعليه لا توجد أي سياسات تخص التسليم والشحن.",
+                textDirection: TextDirection.rtl,
+              ),
+              const SizedBox(height: 600),
+            ],
+          ),
+        ),
+      ),
+
+      // ignore: prefer_const_constructors
+    );
+  }
+}
