36ccfc1ad756ad1c573058e5c0b20aff8ca19d75
shaden789 <52710817+shaden789@users.noreply.github.com>
2022-12-13 06:38:03 +0300

    Food Posts

---
+++ faydh/lib/Database/database.dart
@@ -0,0 +1,46 @@
+import 'dart:core';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _foodPostCollection =
+    _firestore.collection("foodPost");
+
+class Database {
+  static Future<void> addFoodPostData({
+    required BuildContext context,
+    required String docId,
+    required String userUid,
+    required String userPost,
+    required String postTitle,
+    required String postText,
+    required String postAdress,
+    required String postImage,
+    required String postExp,
+    required String food_cont,
+  }) async {
+    DocumentReference documentReference = _foodPostCollection.doc(docId);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      'Cid': userUid.toString().trim(),
+      'docId': docId.toString().trim(),
+      'userPost': userPost.toString().trim(),
+      'postTitle': postTitle.toString().trim(),
+      'postText': postText.toString().trim(),
+      'postAdress': postAdress.toString().trim(),
+      "postImage": postImage.toString().trim(),
+      "pathImage": "pathImage".toString().trim(),
+      "postExp": postExp.toString().trim(),
+      "food_cont": food_cont.toString().trim(),
+      "postDate": DateTime.now().toString(),
+    };
+    await documentReference.set(data).whenComplete(() {
+      print('Note item saved to the database');
+      Fluttertoast.showToast(msg: "تم نشر الاعلان بنجاح");
+      Navigator.pop(context);
+    }).catchError((e) {
+      print(e);
+    });
+  }
+}
---
+++ faydh/lib/widgets/all_foodPosts.dart
@@ -0,0 +1,177 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/post_model.dart';
+
+class AllPostsCard extends StatefulWidget {
+  final snap;
+  final Posts postData;
+
+  AllPostsCard({
+    this.snap,
+    required this.postData,
+    super.key,
+  });
+
+  @override
+  State<AllPostsCard> createState() => _AllPostsCardState();
+}
+
+class _AllPostsCardState extends State<AllPostsCard> {
+  String myUsername = "";
+
+  var seen = false;
+
+  @override
+  void initState() {
+    myUsername = "";
+    //getUser2();
+    // TODO: implement initState
+  }
+
+  Future getUser2() async {
+    if (!seen) {
+      var collection = FirebaseFirestore.instance.collection('posts');
+      var docSnapshot =
+          await collection.doc("${widget.snap["userId"].toString()}").get();
+      if (docSnapshot != null && mounted) {
+        Map<String, dynamic>? data = docSnapshot.data();
+        var _value = data?['username'];
+        setState(() {
+          myUsername = "";
+          myUsername = _value.toString();
+        });
+        //myUsername = _value.toString() ;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //var _dta = "${widget.snap["postImage"].toString()}";
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            width: 0,
+            color: Colors.white,
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        margin: const EdgeInsets.all(8),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  tweetAvatar(),
+                  tweetBody(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget tweetAvatar() {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: const CircleAvatar(
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.account_circle_rounded,
+          color: Color.fromARGB(226, 29, 92, 76),
+          size: 40,
+        ),
+      ),
+    );
+  }
+
+  Widget tweetBody() {
+    //var _dta = "${widget.snap["postImage"].toString()}";
+    var _dta = widget.postData.postImage.toString();
+
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          tweetHeader(),
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 8, right: 2, bottom: 2, left: 2),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(top: 20, right: 0, bottom: 2, left: 50),
+              child: Text(
+                  //"${widget.snap["postText"].toString()}",
+                  widget.postData.postText.toString(),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          if (_dta.isNotEmpty)
+            SizedBox(
+              width: 250,
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: Colors.grey,
+                    child: Image(
+                      image: NetworkImage(_dta),
+                      fit: BoxFit.cover,
+                      height: 150,
+                      width: 250,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget tweetHeader() {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(right: 5.0),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Row(
+              children: [
+                Text(
+                  //"${widget.snap["postUserName"].toString()}",
+                  widget.postData.postUserName.toString(),
+                  //myUsername,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  widget.postData.postText.toString(),
+                ),
+              ],
+            ),
+          ),
+        ),
+        //  Spacer(),
+      ],
+    );
+  }
+}
