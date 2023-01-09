8f94a21db70b78f38b6ce98d90941a3f9be1d192
ishaikha1 <ishaikha177@gmail.com>
2023-01-09 23:44:16 +0300

    Provider edit

--- faydh/lib/ReservedListProvider.dart
+++ faydh/lib/ReservedListProvider.dart
@@ -14,78 +14,83 @@ class ReservedProviderScreen extends StatefulWidget {
   @override
   State<ReservedProviderScreen> createState() => _ReservedProviderScreenState();
 }
-  String id = FirebaseAuth.instance.currentUser!.uid;
+
+String id = FirebaseAuth.instance.currentUser!.uid;
 
 class _ReservedProviderScreenState extends State<ReservedProviderScreen> {
-  
-  
-  
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Color(0xffd6ecd0),
-      appBar: AppBar(
-          elevation: 2.0,
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color(0xFF1A4D2E),
-          title: const Text("الطلبات المحجوزة"),
-          leading: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: const Icon(
-              Icons.arrow_back_ios_rounded,
-              color: Color.fromARGB(225, 255, 255, 255),
-            ),
-          )),
-      body: SafeArea(
-        child: Container(
+        backgroundColor: Color(0xffd6ecd0),
+        appBar: AppBar(
+            elevation: 2.0,
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: const Color(0xFF1A4D2E),
+            title: const Text("الطلبات المحجوزة"),
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(
+                Icons.arrow_back_ios_rounded,
+                color: Color.fromARGB(225, 255, 255, 255),
+              ),
+            )),
+        body: SafeArea(
+          child: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                   begin: Alignment.bottomLeft,
                   end: Alignment.topRight,
-                  stops: [0.1, 0.9],
-                  colors: [Color.fromARGB(142, 47, 101, 69), Color(0xffd6ecd0)]),
+                  stops: [
+                    0.1,
+                    0.9
+                  ],
+                  colors: [
+                    Color.fromARGB(142, 47, 101, 69),
+                    Color(0xffd6ecd0)
+                  ]),
             ),
             child: StreamBuilder(
-              stream: FirebaseFirestore.instance
-           .collection('foodPost')
-           .where('Cid', isEqualTo: id)
-           .where('reserve', isEqualTo: "1")
-           .orderBy(
-            "docId",
-           descending: true,
-           )
-           .snapshots(),
-              builder:(context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snaphot) {
-                if (snaphot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      color: Colors.green,
+                stream: FirebaseFirestore.instance
+                    .collection('foodPost')
+                    .where('Cid',
+                        isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                    .where('reserve', isEqualTo: "1")
+                    .orderBy(
+                      "docId",
+                      descending: true,
+                    )
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snaphot) {
+                  if (snaphot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.green,
+                      ),
+                    );
+                  }
+                  if (snaphot.hasData && snaphot.data?.size == 0) {
+                    return const Center(
+                        child: Text(" لا توجد أي حجوزات بعد",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            )));
+                  }
+
+                  return ListView.builder(
+                    itemCount: snaphot.data?.docs.length,
+                    itemBuilder: (context, index) => ProviderRlistCard(
+                      snap: snaphot.data?.docs[index].data(),
                     ),
                   );
-                }if (snaphot.hasData && snaphot.data?.size == 0) {
-                  return const Center(
-                      child: Text(" لا توجد أي حجوزات بعد",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                          )));
-                }
-              
-                return ListView.builder(
-                  itemCount: snaphot.data?.docs.length,
-                  itemBuilder: (context, index) => ProviderRlistCard(
-                   snap: snaphot.data?.docs[index].data(),
-                  ),
-                );
-                  }
-            
-            ),),
-     ) );
+                }),
+          ),
+        ));
   }
 }
